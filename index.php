0f7e62e2561515babb9eae90d40b10d1a0007955
Jovan Komazec <komazec92@gmail.com>
2017-11-06 19:43:41 +0000

    Dodavanje HTML-a

--- index.php
+++ index.php
@@ -1,5 +1,24 @@
-<?php
+<html>
+    <head>
+            <title>This is my php website.</title>
+    </head>
 
-echo "This is my first PHP file!"
-
-?>
+</html>
+<body>
+<center>
+     <h1>
+         
+      
+     <br> 
+     </br>
+            <?php
+            // This is my comment
+            echo "This is my first PHP file!"
+            /*
+            This is for multiline comments, anything that
+            we type here, will be commented"
+            */
+            ?>
+     </h1>
+</body>
+</center>
